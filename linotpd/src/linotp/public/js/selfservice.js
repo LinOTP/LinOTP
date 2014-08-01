2ab3c49daf43d1081a2b15f47d4285bd7be170a5
Omar Kohl <omar.kohl@lsexperts.de>
2014-08-01 11:27:34 +0200

    Remove possibly obsolete selfservice JS code
    The code was introduced with commit
    c393b1d3f07014ce2004b0d5b2a38a604bacbdaa in the old repository.
    It isn't clear what the code was supposed to do nor if it ever worked
    
    Change-Id: Ie8f88eafa908cab13805f5969b13ab165dcf168e

--- linotpd/src/linotp/public/js/selfservice.js
+++ linotpd/src/linotp/public/js/selfservice.js
@@ -823,31 +823,6 @@ $(document).ready(function() {
     });
 });
 
-function deselect() {
-    $(".pop").slideFadeToggle(function() {
-        $("#contact").removeClass("selected");
-    });
-}
-
-$(function() {
-    $("#contact").live('click', function() {
-        if ($(this).hasClass("selected")) {
-            deselect();
-        } else {
-            $(this).addClass("selected");
-            $(".pop").slideFadeToggle(function() {
-                $("#email").focus();
-            });
-        }
-        return false;
-    });
-
-    $(".close").live('click', function() {
-        deselect();
-        return false;
-    });
-});
-
 $.fn.slideFadeToggle = function(easing, callback) {
     return this.animate({
         opacity : 'toggle',
