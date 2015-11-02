c0e657f6fec53dd190e185ed1ddc0319c5f4cba6
Christian Pommranz <christian.pommranz@lsexperts.de>
2015-11-02 17:34:42 +0100

    Set version 2.7.1.3
    Change-Id: I43c1640620b17484a31b23da499bf868bb673571

--- linotpd/src/setup.py
+++ linotpd/src/setup.py
@@ -37,7 +37,7 @@ def get_debian_package():
 
 setup(
     name='LinOTP',
-    version='2.7.1.2',
+    version='2.7.1.3',
     description='LinOTP Service',
     author='LSE Leading Security Experts GmbH',
     license='AGPL v3, (C) LSE Leading Security Experts GmbH',
