2559c3de2dc2b37e07988cd1ef1e0d0372a475b2
Christian Pommranz <christian.pommranz@lsexperts.de>
2015-12-17 11:07:13 +0100

    Revert "[2.8.0.3] removed the usage of the default port 5001 in sms for httpsms"
    This reverts commit c69647a631115a2223da65c2a338ab6fdd57ea65.
    
    Change-Id: I509fe5bcf21279bd1f351460eaffe6b61449dc45

--- linotpd/src/linotp/tests/functional_special/test_httpsms.py
+++ linotpd/src/linotp/tests/functional_special/test_httpsms.py
@@ -442,8 +442,7 @@ class TestHttpSmsController(TestController):
         """
         use the internal testing server for
         """
-        sms_conf = {"URL": "http://localhost:%s/testing/http2sms"
-                        % self.paster_port,
+        sms_conf = {"URL": "http://localhost:5001/testing/http2sms",
                     "PARAMETER": {"account": "clickatel", "username": "legit"},
                     "SMS_TEXT_KEY": "text",
                     "SMS_PHONENUMBER_KEY": "destination",
