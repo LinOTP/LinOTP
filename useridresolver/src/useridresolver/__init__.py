2c3e863e111eaff807ee343532d89eb64123f8cc
Christian Pommranz <christian.pommranz@lsexperts.de>
2016-06-10 13:41:46 +0200

    Set version 2.8.1.2
    Change-Id: Id690e007d8e9d92ab95dbba6bc2d0d531b8bf144

--- useridresolver/src/useridresolver/__init__.py
+++ useridresolver/src/useridresolver/__init__.py
@@ -42,4 +42,4 @@ __copyright__ = "Copyright (C) 2010 - 2016 LSE Leading Security Experts GmbH"
 __license__ = "Gnu AGPLv3"
 __contact__ = "www.linotp.org"
 __email__ = "linotp@lsexperts.de"
-__version__ = '2.8.1.2rc3'
+__version__ = '2.8.1.2'
