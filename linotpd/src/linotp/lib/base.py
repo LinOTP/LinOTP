0ef952209e0aadd17dbd260f1bc4ded4493b2e68
Kay Winkler <kay.winkler@lsexperts.de>
2016-06-13 16:08:26 +0200

    fix for breaking qrtoken tests
    for the qrtoken functionality the ec keys are created on the first server
    start. But during test runs the database / config entries are deleted
    which as well deletes the ec keys. As the server is not restarted, the
    ec keys are not recreated and the tests are failing.
    The fix is to check for the ec keys now on every request.
    
    Change-Id: I05d2067f5534216260db91347b651b259c39f9bb

--- linotpd/src/linotp/lib/base.py
+++ linotpd/src/linotp/lib/base.py
@@ -330,11 +330,10 @@ class BaseController(WSGIController):
         l_config = initLinotpConfig()
 
         # initialize the elliptic curve secret + public key for the qr token
-        if first_run:
-            linotpQrTokenSecretKey = l_config.get('QrTokenSecretKey.system',
-                                                  False)
-            if not linotpQrTokenSecretKey:
-                init_qrtoken_secret_key(l_config, cert_id='system')
+        linotpQrTokenSecretKey = l_config.get('QrTokenSecretKey.system',
+                                              False)
+        if not linotpQrTokenSecretKey:
+            init_qrtoken_secret_key(l_config, cert_id='system')
 
         resolver_setup_done = config.get('resolver_setup_done', False)
         if resolver_setup_done is False:
