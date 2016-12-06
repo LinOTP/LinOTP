041a8bb12ee3270fd19e272c7b57e56686b155a8
Lukas Engelter <lukas.engelter@lsexperts.de>
2016-12-06 10:02:44 +0100

    [LinOTP] ManageUI: Added helper functions to transform and compare linotp versions
    Two methods were added to extract minor and major linotp versions and
      a comparator method for detecting upgrades
    
    Change-Id: I949b1ccadf12fe3f24ba559e3a6f280ad32779ed

--- linotpd/src/linotp/tests/unit/js/spec/javascripts/linotp_utils_spec.js
+++ linotpd/src/linotp/tests/unit/js/spec/javascripts/linotp_utils_spec.js
@@ -48,3 +48,83 @@ describe("isDefinedKey", function() {
     expect(result_inner).toBe(false);
   });
 });
+
+describe("parseMajorVersionNumber", function() {
+  it("finds the major version", function() {
+    expect(parseMajorVersionNumber("LinOTP 2.9.0")).toBe("2.9");
+    expect(parseMajorVersionNumber("LinOTP 2.9.1")).toBe("2.9");
+    expect(parseMajorVersionNumber("LinOTP 2.9.0.1")).toBe("2.9");
+    expect(parseMajorVersionNumber("LinOTP 2.8.0.1")).toBe("2.8");
+  });
+
+  it("finds the major version for major only number", function() {
+      expect(parseMajorVersionNumber("LinOTP 3.0")).toBe("3.0");
+      expect(parseMajorVersionNumber("LinOTP 2.9")).toBe("2.9");
+  })
+
+  it("allows different text product names", function() {
+      expect(parseMajorVersionNumber("LinOTP Test 3.0.1")).toBe("3.0");
+      expect(parseMajorVersionNumber("LinOTP_Util 2.9")).toBe("2.9");
+  })
+
+  it("cuts off dev release identifiers", function() {
+      expect(parseMajorVersionNumber("LinOTP 2.9.1.dev0")).toBe("2.9");
+      expect(parseMajorVersionNumber("LinOTP 3.0.dev1")).toBe("3.0");
+  })
+
+  it("parses version numbers itself", function() {
+      expect(parseMajorVersionNumber("2.9.1.dev0")).toBe("2.9");
+      expect(parseMajorVersionNumber("2.9.dev0")).toBe("2.9");
+      expect(parseMajorVersionNumber("2.9")).toBe("2.9");
+      expect(parseMajorVersionNumber("2")).toBe("2.0");
+  })
+});
+
+describe("parseMinorVersionNumber", function() {
+  it("finds the minor version", function() {
+    expect(parseMinorVersionNumber("LinOTP 2.9.0")).toBe("2.9.0");
+    expect(parseMinorVersionNumber("LinOTP 2.9.1")).toBe("2.9.1");
+    expect(parseMinorVersionNumber("LinOTP 2.9.0.1")).toBe("2.9.0");
+    expect(parseMinorVersionNumber("LinOTP 2.8.0.1")).toBe("2.8.0");
+  });
+
+  it("finds the minor version for major only number", function() {
+      expect(parseMinorVersionNumber("LinOTP 3.0")).toBe("3.0.0");
+      expect(parseMinorVersionNumber("LinOTP 2.9")).toBe("2.9.0");
+  })
+
+  it("allows different text product names", function() {
+      expect(parseMinorVersionNumber("LinOTP Test 3.0.1")).toBe("3.0.1");
+      expect(parseMinorVersionNumber("LinOTP_Util 2.9")).toBe("2.9.0");
+  })
+
+  it("cuts off dev release identifiers", function() {
+      expect(parseMinorVersionNumber("LinOTP 2.9.1.dev0")).toBe("2.9.1");
+      expect(parseMinorVersionNumber("LinOTP 3.0.dev1")).toBe("3.0.0");
+  })
+
+  it("parses version numbers itself", function() {
+      expect(parseMinorVersionNumber("2.9.1.dev0")).toBe("2.9.1");
+      expect(parseMinorVersionNumber("2.9.dev0")).toBe("2.9.0");
+      expect(parseMinorVersionNumber("2.9")).toBe("2.9.0");
+      expect(parseMinorVersionNumber("2")).toBe("2.0.0");
+  })
+});
+
+describe("compareVersionNumbers", function() {
+  it("compares versions with same length", function() {
+    expect(compareVersionNumbers("3.0.0", "3.0.0")).toBe(0);
+    expect(compareVersionNumbers("3.0.1", "3.0.0")).toBe(1);
+    expect(compareVersionNumbers("3.0.1", "3.0.0")).toBe(1);
+    expect(compareVersionNumbers("2", "3")).toBe(-1);
+    expect(compareVersionNumbers("2.9.0", "2.0.0")).toBe(1);
+  });
+
+  it("compares versions with different length", function() {
+    expect(compareVersionNumbers("3.0.0", "3.0")).toBe(0);
+    expect(compareVersionNumbers("3.0", "3.0.1")).toBe(-1);
+    expect(compareVersionNumbers("2.1", "3")).toBe(-1);
+    expect(compareVersionNumbers("2.1.1", "2.1")).toBe(1);
+  });
+
+});
