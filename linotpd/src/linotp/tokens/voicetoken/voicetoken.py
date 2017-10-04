b04986a0e46829b5408b2cf6b8c9076f88b82ad9
Reinhard Stampp <reinhard.stampp@keyidentity.com>
2017-10-04 15:18:48 +0200

    VoiceToken: Add initial version of  VoiceTokenClass
    Add initial version which implements the __init__
    and the class type and prefix.
    
    Base hash algorithm is sha256, mode is challenge
    
    Change-Id: I24023f9b9e98dfbc2035faac0b40c5758a0d4d3f
    refers: LINOTP-537

--- linotpd/src/linotp/tokens/voicetoken/voicetoken.py
+++ linotpd/src/linotp/tokens/voicetoken/voicetoken.py
@@ -23,3 +23,78 @@
 #    Contact: www.linotp.org
 #    Support: www.keyidentity.com
 #
+
+import datetime
+from linotp.lib.HMAC import HmacOtp
+
+from linotp.lib.user import getUserDetail
+
+from linotp.lib.auth.validate import check_pin
+from linotp.lib.auth.validate import check_otp
+from linotp.lib.auth.validate import split_pin_otp
+
+from linotp.lib.config import getFromConfig
+import binascii
+
+from linotp.lib.policy import getPolicyActionValue
+from linotp.lib.policy import getPolicy, get_client_policy
+from linotp.lib.policy import trigger_phone_call_on_empty_pin
+
+
+from linotp.lib.context import request_context as context
+from linotp.lib.error import ParameterError
+
+from linotp.tokens.hmactoken import HmacTokenClass
+from linotp.tokens import tokenclass_registry
+
+import logging
+
+LOG = logging.getLogger(__name__)
+
+
+@tokenclass_registry.class_entry('voice')
+@tokenclass_registry.class_entry(
+    'linotp.tokens.voicetoken.VoicetokenClass')
+class VoiceTokenClass(HmacTokenClass):
+
+    """
+    Voice token class implementation
+    """
+
+# --------------------------------------------------------------------------- --
+
+    def __init__(self, token_obj):
+        """
+        Constructor for VoiceToken
+        :param token_obj: instance of the orm db object
+        :type token_obj:  orm object
+        """
+        HmacTokenClass.__init__(self, token_obj)
+        self.setType(u'voice')
+        self.hKeyRequired = False # what is hkeyRequired... will be checked
+        # if otpKey is none while updating - By init update will also be called
+        # is hkeyRequired if seed is required by request? Where comes the
+        # param variable from - request context ?
+
+        # we support various hashlib methods, but only one create
+        # which is effectively set in the update
+        self.hashlibStr = getFromConfig("hotp.hashlib", "sha256")
+        # we have no challenge mode
+        self.mode = ['challenge'] # user want to login
+        # challenge is triggered and otp comes via phone call to the user
+
+    @classmethod
+    def getClassType(cls):
+        """
+        getClassType - return the token type shortname
+
+        :return: 'voice'
+        :rtype: string
+
+        """
+        return "voice"
+
+    @classmethod
+    def getClassPrefix(cls):
+        # OATH standard compliant prefix: XXYY XX= vendor, YY - token type
+        return "LSVO"
