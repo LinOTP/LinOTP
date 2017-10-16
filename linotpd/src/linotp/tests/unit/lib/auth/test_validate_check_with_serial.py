57365e1f994745afe4cb793db8095636a6958a7c
Kay Winkler <kay.winkler@keyidentity.com>
2017-10-16 17:44:00 +0200

    [LinOTP] fix for /validate/check with user and serial parameter
    in the case of enrolling and activating an qr ot push token, the
    serial number is provided when calling the /validate/check.
    In case of a serial number and the user are in the calling args
    the evaluation should only be restricted to the token with the
    serial number while verifiying the user password/pin.
    
    refers LinOTP-534
    
    Change-Id: I15662787de334cd4c76c34a1963e741b6aa5d689

---
+++ linotpd/src/linotp/tests/unit/lib/auth/test_validate_check_with_serial.py
@@ -0,0 +1,106 @@
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the
+#               GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+#    E-mail: linotp@keyidentity.com
+#    Contact: www.linotp.org
+#    Support: www.keyidentity.com
+#
+"""
+Tests the create of audit entries
+"""
+
+import unittest
+from mock import patch
+from linotp.lib.user import User
+from linotp.lib.auth.validate import ValidationHandler
+
+mocked_context = {'audit': {}}
+
+class mocked_TokenHandler():
+
+    def auto_assign_otp_only(self, *args, **kwargs):
+        return False
+
+class FakeToken():
+
+    pass
+
+def mocked_getTokens4UserOrSerial(
+                               query_user=None,
+                               serial=None,
+                               token_type=None,
+                               read_for_update=True):
+    if serial and not query_user:
+        return [FakeToken()]
+    if not serial and query_user:
+        return [FakeToken(), FakeToken()]
+
+class TestCheckWithSerial(unittest.TestCase):
+    """
+    verify that the calling of checkUserPass with a serial number
+    in the options should focus the search of the tokens only on this token
+    with the serial number.
+    """
+
+    @patch("linotp.lib.auth.validate.context", mocked_context)
+    @patch("linotp.lib.auth.validate.TokenHandler", mocked_TokenHandler)
+    @patch("linotp.lib.auth.validate.getTokens4UserOrSerial",
+           mocked_getTokens4UserOrSerial)
+    @patch("linotp.lib.auth.validate.ValidationHandler.checkTokenList")
+    @patch("linotp.lib.auth.validate.get_auth_forward")
+    @patch("linotp.lib.auth.validate.getUserId")
+    def test_validate_check_with_serial(self, mocked_geUserId,
+                                        mocked_get_auth_forward,
+                                        mocked_checkTokenList,
+                                        ):
+        """
+        test calling checkUserPass with serial in the list of optional args
+        """
+        # ------------------------------------------------------------------ --
+
+        # test setup
+
+        user = User('root', 'anywhere')
+        passw = "Test123!"
+
+        serial = 'tok123'
+        options = {'serial': serial}
+        
+        mocked_geUserId.return_value = ('uid', 'resolver', 'resolverClass')
+        mocked_get_auth_forward.return_value = None
+        mocked_checkTokenList.return_value = True, None
+
+        # ------------------------------------------------------------------ --
+
+        # if serial was given, there should be only one token in the tokenlist
+        # when calling of checkTokenList
+
+        vh = ValidationHandler()
+        _result = vh.checkUserPass(user, passw, options=options)
+
+        call_args = mocked_checkTokenList.call_args
+        token_list = call_args[0][0]
+        assert len(token_list) == 1
+
+        # ------------------------------------------------------------------ --
+
+        # if no serial was given, there should be 2 token in the tokenlist
+        # when calling of checkTokenList
+
+        vh = ValidationHandler()
+        _result = vh.checkUserPass(user, passw, options=None)
+
+        call_args = mocked_checkTokenList.call_args
+        token_list = call_args[0][0]
+        assert len(token_list) > 1
+
+        return
+# eof #
