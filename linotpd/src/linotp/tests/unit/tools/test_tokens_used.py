a92d7e5a3a352d94a57ab13fdf7e6c4b381a6331
Chris Halls <chris.halls@keyidentity.com>
2018-02-02 17:02:58 +0100

    Fix exception in linotp-token-usage/tokens-used
    This was caused by a filename namespace collision for linotp.lib.utils
    
    Added basic unit tests for loading the scripts and linotp-sql-janitor.
    
    LINOTP-731
    
    Change-Id: I8918606366a9b591b769464dca2cca75165d477a

---
+++ linotpd/src/linotp/tests/unit/tools/test_tokens_used.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+#
+#    LinOTP - the open source solution for two factor authentication
+#    Copyright (C) 2018 KeyIdentity GmbH
+#
+#    This file is part of LinOTP server.
+#
+#    This program is free software: you can redistribute it and/or
+#    modify it under the terms of the GNU Affero General Public
+#    License, version 3, as published by the Free Software Foundation.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the
+#               GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+#    E-mail: linotp@keyidentity.com
+#    Contact: www.linotp.org
+#    Support: www.keyidentity.com
+#
+
+from mock import patch
+
+from script_testing_lib import ScriptTester
+
+# -------------------------------------------------------------------------- --
+
+class TestLinotpTokensUsed(ScriptTester):
+
+    script_name = 'linotp-tokens-used'
+
+    def run_main(self, *args):
+        main_args = [self.script_module.__name__]
+        main_args.extend(args)
+
+        with patch('sys.argv', main_args):
+
+            output = []
+            def append_output(print_args):
+                output.append(str(print_args))
+
+            with patch.object(self.script_module, 'print', side_effect=append_output):
+                with patch('sys.exit'):
+                    self.script_module.main()
+
+    def test_main_config(self):
+        with patch.object(self.script_module, 'print_config', wraps=self.script_module.print_config) as mock_config:
+            self.run_main("config")
+            mock_config.assert_called_with()
+
+    @patch('sys.exit')
+    def test_config(self, mock_exit):
+        with patch.object(self.script_module, 'print_config') as mock_config:
+            self.script_module.main()
+
+    def test_token_usage(self):
+        self.script_module.tokens_used(None)
