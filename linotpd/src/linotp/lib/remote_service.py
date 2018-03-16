1182d46d5768ea19dabb4ba3472140eb966026a0
Andreas Rammhold <andreas.rammhold@keyidentity.com>
2018-03-16 21:36:16 +0100

    Add RemoteServiceList and RemoteService for transparent service failover
    RemoteServiceList provides a list of RemoteService's that will
    transparently continue with another RemoteService whenever one fails.
    
    After an exceeding amount of failures with one service it will be marked
    as unavailable for a period of time. Only after that period of time has
    passed a service will be retried and eventually marked as available
    again.
    
    Refers to LINOTP-754
    
    Change-Id: I567eec097df07d1f6d84efb7938d70ca12ea4711

---
+++ linotpd/src/linotp/lib/remote_service.py
@@ -0,0 +1,183 @@
+# -*- coding: utf-8 -*-
+#
+#    LinOTP - the open source solution for two factor authentication
+#    Copyright (C) 2010 - 2018 KeyIdentity GmbH
+#
+#    This file is part of LinOTP server.
+#
+#    This program is free software: you can redistribute it and/or
+#    modify it under the terms of the GNU Affero General Public
+#    License, version 3, as published by the Free Software Foundation.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the
+#               GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+#    E-mail: linotp@keyidentity.com
+#    Contact: www.linotp.org
+#    Support: www.keyidentity.com
+#
+
+from datetime import datetime, timedelta
+
+def now():
+    """
+    Return the current time.
+
+    This function is required for datetime mocking during testing.
+    """
+    return datetime.now()
+
+
+class State(object):
+    """
+    State of a Service
+    """
+    FUNCTIONAL = 1
+    UNAVAILABLE = 2
+
+
+class ServiceUnavailable(Exception):
+    """
+    Thrown when a service is unavailable.
+
+    This exception is used for failover to other services and not passed to
+    users.
+    """
+    pass
+
+
+class AllServicesUnavailable(Exception):
+    """
+    Thrown when all services are unavailable.
+
+    This exception will be passed on to callers of
+    `RemoteService.call_first_available` when there are no functional services
+    left.
+    """
+    pass
+
+
+class RemoteService(object):
+    """
+    A service that keeps track of it failures and marks itself as unavilable
+    after an exceeding amount of configured (base) exceptions.
+    """
+
+    def __init__(self,
+                 func,
+                 failure_threshold=5,
+                 recovery_timeout=30,
+                 expected_exception=Exception):
+
+        self.func = func
+        self.failure_count = 0
+        self.failure_threshold = failure_threshold
+        self.recovery_timeout = recovery_timeout
+        self.expected_exception = expected_exception
+        self.last_unavailable = None
+        self.state = State.FUNCTIONAL
+
+    def on_recovery(self):
+        self.failure_count = 0
+        self.state = State.FUNCTIONAL
+
+    def on_failure(self):
+        self.failure_count += 1
+        if self.failure_count >= self.failure_threshold:
+            self.last_unavailable = now()
+            self.state = State.UNAVAILABLE
+
+    def __call__(self, *args, **kwargs):
+        """
+        Calls the wrapped function
+
+        :raises ServiceUnavailable: If function call did not succeed or
+            recovery time for the function has not passed.
+        """
+
+        if self.state == State.UNAVAILABLE:
+
+            if now() > self.last_unavailable + \
+                                timedelta(seconds=self.recovery_timeout):
+
+                # recovery time is over. try once(!) if function
+                # is available again
+
+                try:
+                    result = self.func(*args, **kwargs)
+                    self.on_recovery()
+                    return result
+                except self.expected_exception as e:
+                    self.on_failure()
+                    raise ServiceUnavailable(repr(e))
+
+            else:
+
+                # recovery time is not over.
+                # simply raise exception
+
+                raise ServiceUnavailable()
+
+        # service state is FUNCTIONAL
+
+        try:
+            result = self.func(*args, **kwargs)
+            return result
+        except self.expected_exception as e:
+            self.on_failure()
+            raise ServiceUnavailable(repr(e))
+
+
+class RemoteServiceList(list):
+    """
+    A list of services that will transparently failover to the next service if one failes.
+    """
+    def __init__(self,
+                 failure_threshold=5,
+                 recovery_timeout=30,
+                 expected_exception=Exception):
+
+        self.failure_threshold = failure_threshold
+        self.recovery_timeout = recovery_timeout
+        self.expected_exception = expected_exception
+
+    def append(self, func, **kwargs):
+
+        service_kwargs = {'failure_threshold': self.failure_threshold,
+                          'recovery_timeout': self.recovery_timeout,
+                          'expected_exception': self.expected_exception}
+
+        service_kwargs.update(kwargs)
+
+        service = RemoteService(func, **service_kwargs)
+        list.append(self, service)
+
+    def call_first_available(self, *args, **kwargs):
+
+        """
+        calls the first available service with the supplied
+        arguments.
+
+        :raises AllServicesUnavailable: If no service is available
+            at the moment
+        """
+
+        for service in self:
+
+            try:
+                result = service(*args, **kwargs)
+                return result
+            except ServiceUnavailable:
+                continue
+
+        else:
+
+            # no service in list succeeded
+            raise AllServicesUnavailable()
---
+++ linotpd/src/linotp/tests/unit/test_remote_service.py
@@ -0,0 +1,204 @@
+# -*- coding: utf-8 -*-
+#
+#    LinOTP - the open source solution for two factor authentication
+#    Copyright (C) 2010 - 2018 KeyIdentity GmbH
+#
+#    This file is part of LinOTP server.
+#
+#    This program is free software: you can redistribute it and/or
+#    modify it under the terms of the GNU Affero General Public
+#    License, version 3, as published by the Free Software Foundation.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the
+#               GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+#    E-mail: linotp@keyidentity.com
+#    Contact: www.linotp.org
+#    Support: www.keyidentity.com
+#
+
+from datetime import datetime, timedelta
+from mock import patch
+from unittest import TestCase
+from linotp.lib.remote_service import RemoteServiceList, AllServicesUnavailable, State
+
+
+class CustomException(Exception):
+    pass
+
+
+def generate_variadic_func(exception=Exception, default=False):
+    def func(*args, **kwargs):
+        func.call_count += 1
+        if func.fail == True:
+            raise exception()
+
+        return args, kwargs
+    func.fail = default
+    func.call_count = 0
+
+    return func
+
+def generate_failing_func(exception=Exception):
+    return generate_variadic_func(exception=exception, default=True)
+
+def generate_passthru_func():
+    return generate_variadic_func(default=False)
+
+
+class TestRemoteServiceList(TestCase):
+
+    def test_emtpy_remote_service_list(self):
+        """
+        A list without services should throw an AllServicesUnavailable exception
+        """
+        services = RemoteServiceList()
+        with self.assertRaises(AllServicesUnavailable):
+            services.call_first_available()
+
+    def test_failing_service_list_should_throw(self):
+        """
+        A list of failing services should throw an AllServicesUnavailable exception
+        """
+        services = RemoteServiceList()
+        services.append(generate_failing_func())
+        services.append(generate_failing_func())
+
+        with self.assertRaises(AllServicesUnavailable):
+            services.call_first_available()
+
+    def test_passes_arguments(self):
+        """
+        A service should get arguments passed on
+        """
+        services = RemoteServiceList()
+        services.append(lambda arg: arg)
+        self.assertEqual(services.call_first_available(42), 42)
+
+    def test_passes_kwargs(self):
+        """
+        A function should get keyword-arguments passed on
+        """
+        services = RemoteServiceList()
+        services.append(generate_passthru_func())
+
+        # the arguments we pass into the service should be returned for investigation
+        args, kwargs = services.call_first_available(1, 2, 3, one=1, two=2, three=3)
+
+        self.assertEqual(args, (1,2,3))
+        self.assertEqual(kwargs, dict(one=1, two=2, three=3))
+
+    def test_service_failover(self):
+        """
+        Services that fail should cause a failover to another service on first failure
+        """
+        services = RemoteServiceList()
+        func = generate_failing_func()
+        services.append(func)
+        services.append(lambda: 42)
+
+        self.assertEqual(services.call_first_available(), 42)
+        self.assertEqual(func.call_count, 1)
+
+    def test_custom_exception_handling(self):
+        """
+        A custom exception should be caught successfully
+        """
+        services = RemoteServiceList(expected_exception=CustomException)
+        func = generate_failing_func(exception=CustomException)
+        services.append(func)
+        services.append(lambda: 23)
+
+        self.assertEqual(services.call_first_available(), 23)
+        self.assertEqual(func.call_count, 1)
+
+    def test_service_is_marked_as_unavailable(self):
+        """
+        Verify that a function that is failing `failure_threshold` times is
+        marked as broken and skipped
+        """
+
+        # create a list of service where the first function yields an exception
+        services = RemoteServiceList(expected_exception=CustomException)
+        func = generate_failing_func(exception=CustomException)
+        services.append(func)
+        services.append(lambda: 42)
+
+        # initially all services should be marked as functional
+        self.assertEqual(services[0].state, State.FUNCTIONAL)
+        self.assertEqual(services[1].state, State.FUNCTIONAL)
+
+        # after calling for `failure_threshold` times the failing service
+        # should be marked as UNAVAILABLE
+        for _ in range(0, services.failure_threshold):
+            self.assertEqual(services.call_first_available(), 42)
+
+        self.assertGreater(func.call_count, 0)
+        self.assertEqual(func.call_count, services.failure_threshold)
+        self.assertEqual(services[0].state, State.UNAVAILABLE)
+
+        # the second function must still be FUNCTIONAL
+        self.assertEqual(services[1].state, State.FUNCTIONAL)
+
+        # and return the expected value
+        self.assertEqual(services.call_first_available(), 42)
+
+    @patch('linotp.lib.remote_service.now')
+    def test_recovery(self, dt_now):
+        """
+        Ensure that a failing function recovers after the configured timeout.
+        Before the timeout expires other good functions should be used.
+        """
+
+        # build a service list with a variadic function & a lambda that tells
+        # us that we are in failover mode
+        services = RemoteServiceList(expected_exception=CustomException)
+        func = generate_variadic_func(exception=CustomException)
+        services.append(func)
+        services.append(lambda: 'failover')
+
+        # record the current time for our mocking
+        start_time = datetime.now()
+
+        # initially return the current time
+        dt_now.return_value = start_time
+
+        # calling the function (before it is marked as failing) returns the
+        # input parameters (as expected)
+        self.assertEqual(services.call_first_available(1), ((1,), {}))
+
+        # mark the first function in the list as failing & move into the future
+        func.fail = True
+        dt_now.return_value += timedelta(seconds=1)
+
+        # call n times until the function is marked as failed
+        for _ in range(0, services.failure_threshold):
+            self.assertEqual(services.call_first_available(), 'failover')
+
+        self.assertEqual(services[0].state, State.UNAVAILABLE)
+
+        # Every second until recovery call the first available service
+        # The return value should always be 'failover'
+        while dt_now.return_value <= start_time + timedelta(seconds=services.recovery_timeout):
+            self.assertEqual(services.call_first_available(), 'failover')
+            dt_now.return_value += timedelta(seconds=1)
+
+        # the state of the primary function should still be UNAVAILABLE
+        self.assertEqual(services[0].state, State.UNAVAILABLE)
+        self.assertEqual(services[1].state, State.FUNCTIONAL)
+
+        # tell function to return again
+        func.fail = False
+
+        # after the recovery timeout the first service should start returing again
+        dt_now.return_value += timedelta(seconds=1)
+        self.assertEqual(services.call_first_available(1), ((1,), {}))
+        self.assertEqual(services[0].state, State.FUNCTIONAL)
+
