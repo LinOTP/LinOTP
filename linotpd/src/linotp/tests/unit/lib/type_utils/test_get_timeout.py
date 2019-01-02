06ef4baf64f711d759d1e856966321780502f370
Kay Winkler <kay.winkler@keyidentity.com>
2019-01-02 14:09:31 +0100

    feat: timeout value parsing and type conversion
    timeout is either a float or a tuple of two floats
    which is required for various network connections.
    therefor the parser has been included in the type
    utils lib.
    
    Change-Id: I273d2e6158788839e3ec875507067866386d2c3f

---
+++ linotpd/src/linotp/tests/unit/lib/type_utils/test_get_timeout.py
@@ -0,0 +1,45 @@
+
+import unittest
+from datetime import datetime
+
+from linotp.lib.type_utils import get_timeout
+
+class GetTimeoutTest(unittest.TestCase):
+
+    def test_get_timout_str(self):
+
+        assert get_timeout("5") == 5.0
+
+        assert get_timeout("5 , ") == 5.0
+
+        assert get_timeout("5, 3") == (5.0, 3.0)
+
+        assert get_timeout("5, 3 , ") == (5.0, 3.0)
+
+    def test_get_timout_types(self):
+
+        assert get_timeout((5,2)) == (5,2)
+
+        assert get_timeout(5.0) == 5.0
+
+        assert get_timeout(5) == 5
+
+
+    def test_get_timeout_fail_type(self):
+
+        with self.assertRaises(ValueError) as exx:
+            get_timeout(datetime.now())
+
+        assert "Unsupported timeout input type" in exx.exception.args[0]
+
+    def test_get_timeout_fail_string(self):
+
+        with self.assertRaises(ValueError) as exx:
+            get_timeout("5 , , ,")
+
+        assert "Failed to convert timeout" in exx.exception.message
+
+        with self.assertRaises(ValueError) as exx:
+            get_timeout("5 , 3.0,     ,")
+
+        assert "Failed to convert timeout" in exx.exception.message
