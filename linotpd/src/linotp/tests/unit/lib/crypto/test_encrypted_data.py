4675c20e509259578aeae292127be153612705d6
Chris Halls <chris.halls@keyidentity.com>
2019-08-30 13:21:50 +0200

    fix: Ensure EncryptedData does not generate warnings
    Previously, the constructor was calling __init__ on the derived
    string. However, that was too late as the object was immuatble.
    The correct place is to use __new__, as documented in
    
    https://docs.python.org/3/reference/datamodel.html#basic-customization
    
    Also, added some unit tests to verify the behaviour.

---
+++ linotpd/src/linotp/tests/unit/lib/crypto/test_encrypted_data.py
@@ -0,0 +1,60 @@
+#
+#    LinOTP - the open source solution for two factor authentication
+#    Copyright (C) 2010 - 2019 KeyIdentity GmbH
+#
+#    This file is part of LinOTP server.
+#
+#    This program is free software: you can redistribute it and/or
+#    modify it under the terms of the GNU Affero General Public
+#    License, version 3, as published by the Free Software Foundation.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the
+#               GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+#    E-mail: linotp@keyidentity.com
+#    Contact: www.linotp.org
+#    Support: www.keyidentity.com
+#
+"""
+Tests for EncryptedData
+"""
+
+import pytest
+
+from linotp.lib.crypto.encrypted_data import EncryptedData
+
+TEST_STRING = "test string to be encrypted"
+
+# pylint:disable=redefined-outer-name
+@pytest.fixture
+def data():
+    """
+    Fixture to return an instance of encryptedData
+
+    The unencrypted string is TEST_STRING
+    """
+
+    instance = EncryptedData(TEST_STRING)
+
+    return instance
+
+def test_str(data):
+    """
+    str() should return the string itself
+    """
+    assert str(data) == TEST_STRING
+
+def test_repr(data):
+    """
+    repr() should return a placeholder
+    """
+    rep = repr(data)
+    assert TEST_STRING not in rep
+    assert rep == "XXXXXX"
