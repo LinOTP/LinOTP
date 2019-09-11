71250d3178b293ae6ef341a34aaa35e9c0a3ef44
Chris Halls <chris.halls@keyidentity.com>
2019-09-11 09:26:17 +0200

    refactor: Move controller setup into its own function
    This is to reduce the size of the main app factory.

--- linotpd/src/linotp/app.py
+++ linotpd/src/linotp/app.py
@@ -202,19 +202,30 @@ def create_app(config_name='default', config_extra=None):
 
     app.add_url_rule('/healthcheck/status', 'healthcheck', healthcheck)
 
-    # `CONTROLLERS` is a string that contains a space-separated list
-    # of controllers that should be made available. If an entry in
-    # this list is `foo`, this means that the Python module
-    # `linotp.controllers.foo` should be loaded and its
-    # `FooController` class be made available as a Flask blueprint at
-    # the `/foo` URL prefix. Our dispatch mechanism then ensures that
-    # a request to `/foo/bar` will be dispatched to the
-    # `FooController.bar()` view method.
-    #
-    # In general, controllers may be specified as
-    # `module:url_prefix:class_prefix` (where `url_prefix` and
-    # `class_prefix` are optional and will be constructed from
-    # `module` as above if needed).
+    _setup_controllers(app)
+
+    return app
+
+def _setup_controllers(app):
+    """
+    Initialise controllers and their routing
+
+    `CONTROLLERS` is a string that contains a space-separated list
+    of controllers that should be made available. If an entry in
+    this list is `foo`, this means that the Python module
+    `linotp.controllers.foo` should be loaded and its
+    `FooController` class be made available as a Flask blueprint at
+    the `/foo` URL prefix. Our dispatch mechanism then ensures that
+    a request to `/foo/bar` will be dispatched to the
+    `FooController.bar()` view method.
+
+    In general, controllers may be specified as
+    `module:url_prefix:class_prefix` (where `url_prefix` and
+    `class_prefix` are optional and will be constructed from
+    `module` as above if needed).
+
+    This function should be called during application setup
+        """
 
     for ctrl_name in app.config["CONTROLLERS"].split():
         bits = ctrl_name.split(':', 2)
@@ -234,7 +245,7 @@ def create_app(config_name='default', config_extra=None):
         if cls is None:
             raise ConfigurationError(
                 "{} does not define the '{}' class".format(ctrl_name,
-                                                           ctrl_class_name))
+                                                              ctrl_class_name))
         app.logger.debug(
             "Registering {0} class at {1}".format(ctrl_class_name, url_prefix))
         app.register_blueprint(cls(ctrl_name), url_prefix=url_prefix)
