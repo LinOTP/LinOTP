dbae3dff5d9fd07861b5e0e3bf07629a460d1bf7
Chris Halls <chris.halls@keyidentity.com>
2019-09-26 15:47:26 +0200

    feature: LinOTPApp Flask class
    We introduce our own class to represent the LinOTP application itself

--- linotpd/src/linotp/app.py
+++ linotpd/src/linotp/app.py
@@ -56,6 +56,13 @@ mako = MakoTemplates()
 class ConfigurationError(Exception):
     pass
 
+class LinOTPApp(Flask):
+    """
+    The main LinOTP Flask application instance
+    """
+    def __init__(self):
+        super(LinOTPApp, self).__init__(__name__, static_folder='public', static_url_path='/static')
+
 
 def init_logging(app):
     """Sets up logging for LinOTP."""
@@ -204,7 +211,7 @@ def create_app(config_name='default', config_extra=None):
     @param config_name The name of the configuration to load from settings.py
     @param config_extra An optional dict of configuration override values
     """
-    app = Flask(__name__, static_folder='public', static_url_path='/static')
+    app = LinOTPApp()
 
     app.config.from_object(configs[config_name])
     configs[config_name].init_app(app)
