f16e9446c58533226a804ca265f03dae38f59b85
Kay Winkler <kay.winkler@keyidentity.com>
2019-10-09 15:08:43 +0200

    fix: for get otp to use the utc as base for otp calculation
    code change covered by test_getotp.test_08_getmultiotp()
    
    refers to LINOTP-1155

--- linotpd/src/linotp/tokens/totptoken.py
+++ linotpd/src/linotp/tokens/totptoken.py
@@ -675,7 +675,7 @@ class TimeHmacTokenClass(HmacTokenClass):
 
         hmac2Otp = HmacOtp(secObj, self.getOtpCount(), otplen, self.getHashlib(self.hashlibStr))
 
-        tCounter = self.time2float(datetime.datetime.now())
+        tCounter = self.time2float(datetime.datetime.utcnow())
         if curTime:
             tCounter = self.time2float(curTime)
 
@@ -717,7 +717,7 @@ class TimeHmacTokenClass(HmacTokenClass):
         hmac2Otp = HmacOtp(secObj, self.getOtpCount(),
                            otplen, self.getHashlib(self.hashlibStr))
 
-        tCounter = self.time2float(datetime.datetime.now())
+        tCounter = self.time2float(datetime.datetime.utcnow())
         if curTime:
             tCounter = self.time2float(curTime)
 
