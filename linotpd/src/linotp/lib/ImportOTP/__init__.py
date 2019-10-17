2d84c828290a376485d49e903204e9891a6cea17
Kay Winkler <kay.winkler@keyidentity.com>
2019-10-17 17:33:02 +0200

    chore: some whitespace formatting

--- linotpd/src/linotp/lib/ImportOTP/__init__.py
+++ linotpd/src/linotp/lib/ImportOTP/__init__.py
@@ -321,14 +321,17 @@ def parseYubicoCSV(csv):
                     '''
                     ttype = "hmac"
                     otplen = 6
+
                     if l and len(l) >= 11 and l[11] and l[11].strip():
                         otplen = int(l[11])
+
                     serial = "UBOM%08d_%s" % (serial_int, slot)
-                    TOKENS[serial] = { 'type' : ttype,
-                               'hmac_key' : key,
-                               'otplen' : otplen,
-                               'description': public_id
-                              }
+                    TOKENS[serial] = {
+                        'type' : ttype,
+                        'hmac_key' : key,
+                        'otplen' : otplen,
+                        'description': public_id
+                        }
                 else:
                     log.warning("[parseYubicoCSV] at the moment we do only support Yubico OTP and HOTP: %r" % line)
                     continue
