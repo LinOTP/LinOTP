314e528f7ffd2dadae6b189ae0a2b55c1903d850
Ana Barroso <ana.barroso@keyidentity.com>
2019-10-31 10:36:13 +0100

    fix: modhex string encoding

---
+++ linotpd/src/linotp/tests/unit/lib/util/test_modhex_conversion.py
@@ -0,0 +1,13 @@
+from linotp.lib.util import modhex_decode, modhex_encode
+
+
+def test_mod2hex():
+    m = 'fifjgjgkhchb'
+    h = '474858596061'
+    assert modhex_encode(h) == m
+
+
+def test_hex2mod():
+    m = 'fifjgjgkhchb'
+    h = '474858596061'
+    assert modhex_decode(m) == h
