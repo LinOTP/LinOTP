eab2a844a647b992fd0cfbdaa149ba54544edd43
Reinhard Spies <reinhard.spies@keyidentity.com>
2019-12-09 15:03:50 +0100

    Set version to 2.11.0+custom01

--- adminclient/LinOTPAdminClientCLI/src/linotpadminclientcli/__init__.py
+++ adminclient/LinOTPAdminClientCLI/src/linotpadminclientcli/__init__.py
@@ -37,4 +37,4 @@ __copyright__ = "Copyright (C) 2010 - 2019 KeyIdentity GmbH"
 __license__ = "Gnu AGPLv3"
 __contact__ = "www.linotp.org"
 __email__ = "linotp@keyidentity.com"
-__version__ = '2.11'
+__version__ = '2.11.0+custom01'
