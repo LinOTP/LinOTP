fa73c8ed39a1994f138adcaea986c49dcf6aaf42
Kay Winkler <kay.winkler@keyidentity.com>
2020-02-03 13:33:09 +0100

    feature: add totp token test for userservice/verify

--- linotpd/src/linotp/tests/functional/test_userservice/test_verify_token.py
+++ linotpd/src/linotp/tests/functional/test_userservice/test_verify_token.py
@@ -25,13 +25,23 @@
 #
 
 
-import pytest
+import math
 import binascii
+from datetime import datetime
 from hashlib import sha1
 
 
 from linotp.tests import TestController
 from linotp.lib.HMAC import HmacOtp
+
+unix_start_time = datetime(year=1970, month=1, day=1)
+
+
+def time2counter(t_time, t_step=60):
+    t_delta = (t_time - unix_start_time).total_seconds()
+    counts = t_delta / t_step
+
+    return math.floor(counts)
 
 
 def get_otp(key, counter=None, digits=8):
@@ -64,7 +74,7 @@ class TestUserserviceTokenTest(TestController):
     def tearDown(self):
         TestController.tearDown(self)
 
-    def test_verify_token(self):
+    def test_verify_hmac_token(self):
 
         policy = {
             'name': 'T1',
@@ -92,6 +102,43 @@ class TestUserserviceTokenTest(TestController):
         _, _, seed = seed_value.partition('//')
 
         otp = get_otp(seed, 1, digits=6)
+
+        params = {'serial': serial, 'otp': otp}
+        response = self.make_userselfservice_request(
+            'verify', params=params, auth_user=auth_user)
+
+        assert 'false' not in response
+
+    def test_verify_totp_token(self):
+
+        policy = {
+            'name': 'T1',
+            'action': 'enrollTOTP, delete, history, verify,',
+            'user': ' passthru.*.myDefRes:',
+            'realm': '*',
+            'scope': 'selfservice'
+        }
+        response = self.make_system_request('setPolicy', params=policy)
+        assert 'false' not in response, response
+
+        auth_user = {
+            'login': 'passthru_user1@myDefRealm',
+            'password': 'geheim1'}
+
+        serial = 'totp123'
+
+        params = {'type': 'totp', 'genkey': '1', 'serial': serial}
+        response = self.make_userselfservice_request(
+            'enroll', params=params, auth_user=auth_user, new_auth_cookie=True)
+
+        assert '"img": "<img ' in response, response
+
+        seed_value = response.json['detail']['otpkey']['value']
+        _, _, seed = seed_value.partition('//')
+
+        t_counter = time2counter(t_time=datetime.utcnow(), t_step=30)
+
+        otp = get_otp(seed, counter=t_counter,  digits=6)
 
         params = {'serial': serial, 'otp': otp}
         response = self.make_userselfservice_request(
