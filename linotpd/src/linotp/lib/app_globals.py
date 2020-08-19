56a44da8ac2246e348d089fe5b8c71b14c4a461f
Kay Winkler <kay.winkler@keyidentity.com>
2020-08-19 17:16:52 +0200

    refactor: extract the RWLock as dedicated class

--- linotpd/src/linotp/lib/app_globals.py
+++ linotpd/src/linotp/lib/app_globals.py
@@ -25,9 +25,11 @@
 #
 
 """The application's Globals object"""
-import threading
 import copy
 import logging
+
+
+from linotp.lib.rw_lock import RWLock
 
 log = logging.getLogger(__name__)
 
@@ -143,94 +145,3 @@ class Globals(object):
         self.rwl2.release()
         return self.rcount
 
-
-###
-#Python offers a number of useful synchronization primitives in the threading and Queue modules.
-#One that is missing, however, is a simple reader-writer lock (RWLock). A RWLock allows improved
-#concurrency over a simple mutex, and is useful for objects that have high read-to-write ratios
-#like database caches.
-#
-#Surprisingly, I haven t been able to find any implementation of these semantics, so I rolled my
-#own in a module rwlock.py to implement a RWLock class, along with lock promotion/demotion. Hopefully
-#it can be added to the standard library threading module.
-#This code is hereby placed in the public domain.
-#
-#    from
-#    http://majid.info/blog/a-reader-writer-lock-for-python/
-#
-#
-#   Simple reader-writer locks in Python
-#    Many readers can hold the lock XOR one and only one writer
-#
-#
-###
-#
-#version = """$Id: 04-1.html,v 1.3 2006/12/05 17:45:12 majid Exp $"""
-
-class RWLock:
-    """
-    A simple reader-writer lock Several readers can hold the lock
-    simultaneously, XOR one writer. Write locks have priority over reads to
-    prevent write starvation.
-    """
-    def __init__(self):
-        self.rwlock = 0
-        self.writers_waiting = 0
-        self.monitor = threading.Lock()
-        self.readers_ok = threading.Condition(self.monitor)
-        self.writers_ok = threading.Condition(self.monitor)
-    def acquire_read(self):
-        """Acquire a read lock. Several threads can hold this typeof lock.
-        It is exclusive with write locks.
-        """
-        self.monitor.acquire()
-        while self.rwlock < 0 or self.writers_waiting:
-            self.readers_ok.wait()
-        self.rwlock += 1
-        self.monitor.release()
-    def acquire_write(self):
-        """Acquire a write lock. Only one thread can hold this lock, and
-            only when no read locks are also held.
-        """
-        self.monitor.acquire()
-        while self.rwlock != 0:
-            self.writers_waiting += 1
-            self.writers_ok.wait()
-            self.writers_waiting -= 1
-        self.rwlock = -1
-        self.monitor.release()
-    def promote(self):
-        """Promote an already-acquired read lock to a write lock
-        WARNING: it is very easy to deadlock with this method"""
-        self.monitor.acquire()
-        self.rwlock -= 1
-        while self.rwlock != 0:
-            self.writers_waiting += 1
-            self.writers_ok.wait()
-            self.writers_waiting -= 1
-        self.rwlock = -1
-        self.monitor.release()
-    def demote(self):
-        """Demote an already-acquired write lock to a read lock"""
-        self.monitor.acquire()
-        self.rwlock = 1
-        self.readers_ok.notifyAll()
-        self.monitor.release()
-    def release(self):
-        """Release a lock, whether read or write."""
-        self.monitor.acquire()
-        if self.rwlock < 0:
-            self.rwlock = 0
-        else:
-            self.rwlock -= 1
-        wake_writers = self.writers_waiting and self.rwlock == 0
-        wake_readers = self.writers_waiting == 0
-        self.monitor.release()
-        if wake_writers:
-            self.writers_ok.acquire()
-            self.writers_ok.notify()
-            self.writers_ok.release()
-        elif wake_readers:
-            self.readers_ok.acquire()
-            self.readers_ok.notifyAll()
-            self.readers_ok.release()
