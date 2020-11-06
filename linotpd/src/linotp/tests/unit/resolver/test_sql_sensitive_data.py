fb4e98894f4817bec0062c10bc186786eaa5e06a
Kay Winkler <kay.winkler@keyidentity.com>
2020-11-06 10:59:30 +0100

    test: Fix broken test due to superfluous field
    We remove the user info salt entry, which was not filled
    correctly in the test. Also it is not recommended
    to be used.

--- linotpd/src/linotp/tests/unit/resolver/test_sql_sensitive_data.py
+++ linotpd/src/linotp/tests/unit/resolver/test_sql_sensitive_data.py
@@ -58,7 +58,6 @@ class TestSQLResolverSensitiveData(TestCase):
                         "username": "username",
                         "userid": "id",
                         "password": "password",
-                        "salt": "salt",
                         "givenname": "givenname",
                         "surname": "surname",
                         "email": "email"})
