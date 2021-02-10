96940fd1507a41569a19a59cd95c5bba25691eef
Anselm Lingnau <anselm.lingnau@keyidentity.com>
2021-02-10 13:58:20 +0100

    test: Add test for hyphenated URL in `/auth/challenge-response`.
    This test ensures that `/auth/challenge_response` and
    `/auth/challenge-response` both work, and that their results are the
    same.

---
+++ linotpd/src/linotp/tests/unit/controllers/test_auth.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+#
+#    LinOTP - the open source solution for two factor authentication
+#    Copyright (C) 2010 - 2019 KeyIdentity GmbH
+#
+#    This file is part of LinOTP server.
+#
+#    This program is free software: you can redistribute it and/or
+#    modify it under the terms of the GNU Affero General Public
+#    License, version 3, as published by the Free Software Foundation.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the
+#               GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+#    E-mail: linotp@keyidentity.com
+#    Contact: www.linotp.org
+#    Support: www.keyidentity.com
+#
+
+
+def test_auth_challenge_response_hyphenated(app, client):
+    # Make sure both URLs work and their results are identical
+
+    rv_us = client.get('/auth/challenge_response')
+    assert rv_us.status_code == 200
+    rv_hy = client.get('/auth/challenge-response')
+    assert rv_hy.status_code == 200
+    assert rv_hy.data == rv_us.data
