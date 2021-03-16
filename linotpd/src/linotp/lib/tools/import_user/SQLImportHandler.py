227cf99e1d979aa48b1bc121e823e2a96383bd75
Lukas Engelter <lukas.engelter@keyidentity.com>
2021-03-16 10:02:36 +0100

    refactor: no longer store database connection data for managed resolvers
    Closes LINOTP-1583

--- linotpd/src/linotp/lib/tools/import_user/SQLImportHandler.py
+++ linotpd/src/linotp/lib/tools/import_user/SQLImportHandler.py
@@ -165,8 +165,6 @@ class SQLImportHandler(ImportHandler):
         :return: dictionary of the resolver parameters
         """
 
-        url = self.db_context.engine.url
-
         mapping = {}
         for entry in SQLImportHandler.User.user_entries:
             mapping[entry] = entry
@@ -174,17 +172,17 @@ class SQLImportHandler(ImportHandler):
         where = "groupid = '%s'" % self.groupid
 
         resolver_parameters = {
-            "Driver": url.drivername,
-            "Server": url.host or "",
-            "Port": str(url.port or ""),
-            "Database": url.database,
-            "User": url.username or "",
-            "Password": url.password or "",
+            "Driver": "",
+            "Server": "",
+            "Port": "",
+            "Database": "",
+            "User": "",
+            "Password": "",
             "Table": self.table_name,
             "Where": where,
             "Map": json.dumps(mapping),
             "readonly": True,
-            }
+        }
 
         resolver_parameters['name'] = self.resolver_name
 
