4b54104ae07d1edccd261c41b1f35e0b45c96edc
Mahyar Moghimi <mahyar.moghimi@netgo.de>
2021-09-27 16:27:24 +0200

    test: ensure that imported OATH tokens have valid seeds
    Valid seeds consist only of hexadecimal characters.

--- linotp/tests/unit/tokens/test_hotp.py
+++ linotp/tests/unit/tokens/test_hotp.py
@@ -1,7 +1,9 @@
 import json
+import unittest
 
 from mock import patch
 
+from linotp.tokens.base import InvalidSeedException
 from linotp.tokens.hmactoken import HmacTokenClass
 
 
@@ -31,3 +33,31 @@ def test_hmac_hashlib_sha1(mock_getFromConfig):
     mock_getFromConfig.return_value = "sha1"
     hmac_token = HmacTokenClass(FakeTokenModel())
     assert hmac_token.hashlibStr == "sha1"
+
+
+class HmacTokenValidityTest(unittest.TestCase):
+    @patch("linotp.tokens.hmactoken.getFromConfig")
+    def test_validate_seed(self, moch_getFromConfig):
+        """provided seed should be a valid HEX string"""
+
+        hmac_token = HmacTokenClass(FakeTokenModel())
+        goodseed = "1234ab18790bdef1234"
+        hmac_token.validate_seed(goodseed)
+
+        badseed = "1234ab18790bdef1234g"
+        with self.assertRaises(InvalidSeedException) as cm:
+            hmac_token.validate_seed(badseed)
+        the_exception = cm.exception
+        errormsg = (
+            "The provided token seed contains non-hexadecimal characters"
+        )
+        self.assertEqual(errormsg, the_exception.msg)
+
+        anotherbadseed = "1234ab187fH90bdef1234"
+        with self.assertRaises(InvalidSeedException) as cm:
+            hmac_token.validate_seed(anotherbadseed)
+        the_exception = cm.exception
+        errormsg = (
+            "The provided token seed contains non-hexadecimal characters"
+        )
+        self.assertEqual(errormsg, the_exception.msg)
