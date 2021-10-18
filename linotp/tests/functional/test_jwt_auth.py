70488f612d72770d1fca96e2b9ec7db032f0e5fe
Mahyar Moghimi <mahyar.moghimi@netgo.de>
2021-10-18 17:55:57 +0200

    test: test for expiry and refreshing of jwt authentication

--- linotp/tests/functional/test_jwt_auth.py
+++ linotp/tests/functional/test_jwt_auth.py
@@ -1,15 +1,47 @@
-import logging
-from typing import Callable
+from datetime import datetime, timedelta
+from typing import Callable, Optional
 
 import pytest
 from flask_jwt_extended import create_access_token
+from freezegun import freeze_time
 
+from flask import Response
 from flask.testing import FlaskClient
 
-log = logging.getLogger(__name__)
+from linotp.app import LinOTPApp
 
 
 class TestJwtAdmin:
+    def extract_cookie(
+        self,
+        client: FlaskClient,
+        cookie_name: str,
+    ) -> Optional[str]:
+
+        cookies = client.cookie_jar._cookies.get("localhost.local").get("/")
+
+        if cookie_name in cookies:
+            return cookies[cookie_name].value
+        else:
+            return None
+
+    def do_authenticated_request(self, client: FlaskClient) -> Response:
+        """Calls `/admin/show` which requires a valid session
+
+        Args:
+            client (FlaskClient):
+                the client which is used for the request
+
+        Returns:
+            Response:
+                The response object to the request
+        """
+
+        csrf_token = self.extract_cookie(client, "csrf_access_token")
+        res = client.post("/admin/show", headers={"X-CSRF-TOKEN": csrf_token})
+
+        return res
+
     @pytest.mark.parametrize(
         "username,password,expected_message,expected_http_status,expected_status",
         [
@@ -84,10 +116,8 @@ class TestJwtAdmin:
             ),
         )
 
-        csrf_token = (
-            client.cookie_jar._cookies.get("localhost.local")
-            .get("/")["csrf_access_token"]
-            .value
+        csrf_token = self.extract_cookie(
+            client=client, cookie_name="csrf_access_token"
         )
 
         valid_token_req = client.post(
@@ -148,3 +178,128 @@ class TestJwtAdmin:
         assert result["status"] is False
         assert result["error"]["message"] == "Not authenticated"
         assert invalid_token_req.status_code == 401
+
+    def test_expiration(
+        self,
+        base_app: LinOTPApp,
+        create_common_resolvers: Callable,
+        create_common_realms: Callable,
+        scoped_authclient,
+    ) -> None:
+
+        username = "admin"
+        password = "Test123!"
+        with scoped_authclient(verify_jwt=True) as client:
+            initial_time = datetime(
+                year=2021, month=10, day=19, hour=17, minute=39
+            )
+            with freeze_time(initial_time) as frozen_time:
+
+                client.post(
+                    "/admin/login",
+                    data=dict(username=username, password=password),
+                )
+
+                expiry_time = base_app.config["JWT_ACCESS_TOKEN_EXPIRES"]
+                epsilon_t = 5
+                frozen_time.tick(
+                    delta=timedelta(seconds=expiry_time + epsilon_t)
+                )
+
+                response = self.do_authenticated_request(client)
+
+                assert (
+                    response.status_code == 401
+                ), "Jwt token should have expired"
+
+    def test_refresh(
+        self,
+        base_app: LinOTPApp,
+        create_common_resolvers: Callable,
+        create_common_realms: Callable,
+        scoped_authclient,
+    ) -> None:
+
+        username = "admin"
+        password = "Test123!"
+        initial_time = datetime(year=2021, month=10, day=18, hour=12)
+        refresh_time = base_app.config["JWT_ACCESS_TOKEN_REFRESH"]
+        expiry_time = base_app.config["JWT_ACCESS_TOKEN_EXPIRES"]
+        t_epsilon = 5
+
+        with scoped_authclient(verify_jwt=True) as client:
+
+            with freeze_time(initial_time) as frozen_time:
+
+                client.post(
+                    "/admin/login",
+                    data=dict(username=username, password=password),
+                )
+
+                initial_cookie = self.extract_cookie(
+                    client,
+                    "access_token_cookie",
+                )
+
+                # after this time the token should already get refreshed
+                frozen_time.tick(
+                    delta=timedelta(
+                        seconds=expiry_time - refresh_time + t_epsilon
+                    )
+                )
+
+                self.do_authenticated_request(client)
+
+                second_cookie = self.extract_cookie(
+                    client,
+                    "access_token_cookie",
+                )
+
+                assert initial_cookie is not second_cookie
+
+    def test_no_unnecessary_refresh(
+        self,
+        base_app: LinOTPApp,
+        create_common_resolvers: Callable,
+        create_common_realms: Callable,
+        scoped_authclient,
+    ) -> None:
+
+        username = "admin"
+        password = "Test123!"
+        initial_time = datetime(
+            year=2021, month=10, day=19, hour=17, minute=39
+        )
+        refresh_time = base_app.config["JWT_ACCESS_TOKEN_REFRESH"]
+        expiry_time = base_app.config["JWT_ACCESS_TOKEN_EXPIRES"]
+        epsilon_t = 5
+
+        with scoped_authclient(verify_jwt=True) as client:
+            with freeze_time(initial_time) as frozen_time:
+
+                client.post(
+                    "/admin/login",
+                    data=dict(username=username, password=password),
+                )
+
+                initial_cookie = self.extract_cookie(
+                    client,
+                    "access_token_cookie",
+                )
+
+                frozen_time.tick(
+                    delta=timedelta(
+                        seconds=expiry_time - refresh_time - epsilon_t
+                    )
+                )
+
+                self.do_authenticated_request(client)
+
+                second_cookie = self.extract_cookie(
+                    client,
+                    "access_token_cookie",
+                )
+
+                assert (
+                    initial_cookie is second_cookie
+                ), "The JWT cookie should not have been refreshed"
