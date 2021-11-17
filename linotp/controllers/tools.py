4826d96e1447d2e48bbe15632bf1ed20306c9df8
Kay Winkler <kay.winkler@keyidentity.com>
2021-11-17 15:29:04 +0100

    feature: prevent overwriting of local admin resolver via user import.

--- linotp/controllers/tools.py
+++ linotp/controllers/tools.py
@@ -46,7 +46,7 @@ from linotp.lib.policy import (
 )
 from linotp.lib.realm import getRealms
 from linotp.lib.reply import sendError, sendResult
-from linotp.lib.resolver import getResolverList
+from linotp.lib.resolver import DeleteForbiddenError, getResolverList
 from linotp.lib.tools.import_user import (
     DefaultFormatReader,
     PasswdFormatReader,
@@ -213,6 +213,12 @@ class ToolsController(BaseController):
 
                 log.error("Missing parameter: %r", exx)
                 raise ParameterError("Missing parameter: %r" % exx)
+
+            if resolver_name == current_app.config["ADMIN_RESOLVER_NAME"]:
+                raise DeleteForbiddenError(
+                    f"default admin resolver {resolver_name} is not allowed "
+                    "to be overwritten!"
+                )
 
             groupid = resolver_name
 
