f246fe6b2c49ae2fd086da568d7de5b7659601a5
Kay Winkler <kay.winkler@keyidentity.com>
2021-11-24 06:48:17 +0100

    feature: add new linotp command to set and get support license

---
+++ linotp/cli/support_cmd.py
@@ -0,0 +1,107 @@
+# -*- coding: utf-8 -*-
+#
+#    LinOTP - the open source solution for two factor authentication
+#    Copyright (C) 2010 - 2019 KeyIdentity GmbH
+#    Copyright (C) 2020 arxes-tolina GmbH
+#
+#    This file is part of LinOTP server.
+#
+#    This program is free software: you can redistribute it and/or
+#    modify it under the terms of the GNU Affero General Public
+#    License, version 3, as published by the Free Software Foundation.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the
+#               GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+#    E-mail: linotp@keyidentity.com
+#    Contact: www.linotp.org
+#    Support: www.keyidentity.com
+
+"""linotp admin command.
+
+linotp support set --file license_file
+
+"""
+import sys
+
+import click
+
+from flask import current_app
+from flask.cli import AppGroup, with_appcontext
+
+from linotp.lib.support import getSupportLicenseInfo, setSupportLicense
+from linotp.model import db
+
+support_cmds = AppGroup(
+    "support",
+    help="Administrative commands to set and query the linotp support.",
+)
+
+# ------------------------------------------------------------------------- --
+# Command `linotp support set --file support_file`
+# ------------------------------------------------------------------------- --
+
+
+@support_cmds.command("set", help="set linotp support via linotp cli.")
+@click.argument("license_file_name")
+@with_appcontext
+def set_support(license_file_name):
+    """set a linotp support similar to system/setSupport."""
+
+    try:
+
+        with open(license_file_name, "rb") as license_file:
+            license_text = license_file.read()
+
+        session = db.session()
+
+        success, status = setSupportLicense(license_text.decode("utf-8"))
+
+        session.commit()
+
+    except Exception as exx:
+        current_app.echo(f"Setting license could not be completed: {exx}")
+        sys.exit(1)
+
+    if not success:
+        current_app.echo(f"Failed to set license! {status}")
+        sys.exit(1)
+
+    current_app.echo("Successfully set license.")
+    sys.exit(0)
+
+
+# ------------------------------------------------------------------------- --
+# Command `linotp support get`
+# ------------------------------------------------------------------------- --
+
+
+@support_cmds.command("get", help=("get linotp support info."))
+@with_appcontext
+def get_support():
+    """get the linotp support info similar to system/getSupportInfo"""
+
+    try:
+        session = db.session()
+
+        response, _ = getSupportLicenseInfo()
+
+        session.close()
+
+    except Exception as exx:
+        current_app.echo(f"Getting support could not be completed: {exx}")
+        sys.exit(1)
+
+    if not response:
+        current_app.echo("Getting support failed!")
+        sys.exit(1)
+
+    current_app.echo(f"Getting support response: {response}")
+    sys.exit(0)
---
+++ linotp/tests/unit/cli/test_cli_support_cmd.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+#
+#    LinOTP - the open source solution for two factor authentication
+#    Copyright (C) 2010 - 2019 KeyIdentity GmbH
+#
+#    This file is part of LinOTP server.
+#
+#    This program is free software: you can redistribute it and/or
+#    modify it under the terms of the GNU Affero General Public
+#    License, version 3, as published by the Free Software Foundation.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the
+#               GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+#    E-mail: linotp@keyidentity.com
+#    Contact: www.linotp.org
+#    Support: www.keyidentity.com
+#
+"""LinOTP test for `linotp support` command group."""
+
+import os
+from datetime import datetime
+
+import pytest
+from freezegun import freeze_time
+
+from linotp.cli import main as cli_main
+from linotp.tests import TestController
+
+
+@pytest.fixture
+def runner(app):
+    return app.test_cli_runner(mix_stderr=False)
+
+
+def test_expired_support_file(app, runner):
+    """Test the set of an expired license file and get of the support info."""
+
+    support_file = os.path.join(TestController.fixture_path, "expired-lic.pem")
+
+    result = runner.invoke(cli_main, ["support", "set", support_file])
+    assert result.exit_code == 1
+    assert (
+        "Failed to set license! expired - valid till '2017-12-12'"
+    ) in result.stderr
+
+    result = runner.invoke(cli_main, ["support", "get"])
+    assert result.exit_code == 1
+    assert "Getting support failed!" in result.stderr
+
+
+def test_valid_support_file(app, runner):
+    """Test the set of a valid license file and get of the support info."""
+
+    support_file = os.path.join(TestController.fixture_path, "expired-lic.pem")
+
+    license_valid_date = datetime(year=2017, month=11, day=16)
+
+    with freeze_time(license_valid_date):
+        result = runner.invoke(cli_main, ["support", "set", support_file])
+        assert result.exit_code == 0
+        assert "Successfully set license." in result.stderr
+
+        result = runner.invoke(cli_main, ["support", "get"])
+        assert result.exit_code == 0
+        assert "License for LSE LinOTP 2" in result.stderr
