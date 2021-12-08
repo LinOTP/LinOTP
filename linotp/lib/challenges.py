15f20f3a6485230b9bf14b1e711ae9703fc63f75
Mahyar Moghimi <mahyar.moghimi@netgo.de>
2021-12-08 16:47:41 +0100

    refactor: moving the cache_in_request to a common place
    The function cache_in_request can be used as a tool
    anywhere we need caching. Since it is relatively independent
    of other modules of the main program, we can have it as a common tool.

--- linotp/lib/challenges.py
+++ linotp/lib/challenges.py
@@ -34,6 +34,7 @@ from sqlalchemy import and_, desc
 from flask import g
 
 import linotp
+from linotp.lib.cache_utils import cache_in_request
 from linotp.lib.context import request_context as context
 from linotp.model import db
 from linotp.model.challange import Challenge
@@ -336,58 +337,6 @@ class Challenges(object):
         token_key = token.getSerial() if token else None
         options_key = str(options)
         return json.dumps((token_key, transid, options_key, filter_open))
-
-    def cache_in_request(
-        _func=None,
-        *,
-        key_generator=lambda *args, **kwargs: args + tuple(kwargs.items())
-    ):
-        """
-            decorator to use for caching function calls in the request context
-
-        :param key_generator: the function which takes exactly the same arguments
-        as the function being cached and returns a unique key for its output
-        when the key_generator is not passed, it will use args+tuple(kwargs.items()) as the key
-        This can obviously fail if the args are not hashable
-        """
-
-        def cache_in_request_decorator(func_to_cache):
-            @functools.wraps(func_to_cache)
-            def request_cacher(*args, **kwargs):
-
-                cache_name = func_to_cache.__name__ + "_cache"
-                if not context.get(cache_name, None):
-                    context[cache_name] = {}
-
-                cache_key = key_generator(*args, **kwargs)
-
-                log_message = (
-                    "["
-                    + func_to_cache.__name__
-                    + "]:"
-                    + " getting output values from cache"
-                )
-                if cache_key not in context[cache_name]:
-                    log_message = (
-                        "["
-                        + func_to_cache.__name__
-                        + "]:"
-                        + "output values not in cache, getting values from DB"
-                    )
-
-                    context[cache_name][cache_key] = func_to_cache(
-                        *args, **kwargs
-                    )
-
-                log.info(log_message)
-                return context[cache_name][cache_key]
-
-            return request_cacher
-
-        if _func is None:
-            return cache_in_request_decorator
-        else:
-            return cache_in_request_decorator(_func)
 
     @staticmethod
     @cache_in_request(key_generator=_get_challenges_cache_keygen)
