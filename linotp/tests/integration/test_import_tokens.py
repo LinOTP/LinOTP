8e0b0da3b7a7c19af541d1c6379044d313657fb7
Mahyar Moghimi <mahyar.moghimi@netgo.de>
2022-05-25 12:14:14 +0200

    refactor: removing usage of manage_ui fixture
    The manage_ui is already referenceable through the testcase fixture.
    Calling manage_ui in the current state of the code would already need
    a TestCase Object. Therefore it is a more understandable code to you
    the existing object rather thatn having flying fixtures to do the
    referencing for us.

--- linotp/tests/integration/test_import_tokens.py
+++ linotp/tests/integration/test_import_tokens.py
@@ -49,31 +49,33 @@ pytestmark = pytest.mark.usefixtures("musicians_realm")
 
 
 @pytest.fixture
-def aladdin(manage_ui):
-    return TokenImportAladdin(manage_ui)
+def aladdin(testcase):
+    return TokenImportAladdin(testcase.manage_ui)
 
 
 @pytest.fixture
-def oathcsv_importer(manage_ui):
-    return TokenImportOATH(manage_ui)
+def oathcsv_importer(testcase):
+    return TokenImportOATH(testcase.manage_ui)
 
 
-def check_menu_is_closed(manage_ui: ManageUi):
+def check_menu_is_closed(current_manage_ui):
     """Check that import menu is closed.
 
     By checking that the aladdin menu entry is not visible
     """
     # Move the mouse somewhere else to ensure the menu is closed
-    manage_ui.find_by_id("logo").click()
+    current_manage_ui.find_by_id("logo").click()
 
-    manage_ui.wait_for_element_disappearing("#menu_load_aladdin_xml_tokenfile")
+    current_manage_ui.wait_for_element_disappearing(
+        "#menu_load_aladdin_xml_tokenfile"
+    )
 
 
-def test_token_import_aladdin_xml(
-    manage_ui: ManageUi, aladdin: TokenImportAladdin
-):
+def test_token_import_aladdin_xml(testcase, aladdin: TokenImportAladdin):
     """Test import of valid aladdin tokens."""
-    aladdin_xml_path = os.path.join(manage_ui.test_data_dir, "aladdin.xml")
+    aladdin_xml_path = os.path.join(
+        testcase.manage_ui.test_data_dir, "aladdin.xml"
+    )
     aladdin.do_import(file_path=aladdin_xml_path)
 
     token_serials = (
@@ -84,36 +86,38 @@ def test_token_import_aladdin_xml(
     )
 
     # Check the grid lines for the imported tokens
-    assert_tokens_are_in_grid(manage_ui, token_serials, "HMAC")
+    assert_tokens_are_in_grid(testcase.manage_ui, token_serials, "HMAC")
 
     # Check token info contents for one of the tokens
     serial_to_check = token_serials[0]
-    token_info = manage_ui.token_view.get_token_info(serial_to_check)
+    token_info = testcase.manage_ui.token_view.get_token_info(serial_to_check)
     assert token_info["LinOtp.TokenType"] == "HMAC"
     assert token_info["LinOtp.TokenSerialnumber"] == serial_to_check
 
-    check_menu_is_closed(manage_ui)
+    check_menu_is_closed(testcase.manage_ui)
 
 
 def test_token_import_aladdin_invalid_xml(
-    manage_ui: ManageUi, aladdin: TokenImportAladdin
+    testcase, aladdin: TokenImportAladdin
 ):
     """Test import of invalid xml."""
 
     with pytest.raises(TokenImportError):
         aladdin.do_import(
-            file_path=os.path.join(manage_ui.test_data_dir, "wrong_token.xml")
+            file_path=os.path.join(
+                testcase.manage_ui.test_data_dir, "wrong_token.xml"
+            )
         )
-    check_menu_is_closed(manage_ui)
+    check_menu_is_closed(testcase.manage_ui)
 
 
-def test_token_import_oath_csv(
-    manage_ui: ManageUi, oathcsv_importer: TokenImportOATH
-):
+def test_token_import_oath_csv(testcase, oathcsv_importer: TokenImportOATH):
     """Test import of valid oath csv tokens."""
 
     #   data to be tested against:
-    oath_csv_path = os.path.join(manage_ui.test_data_dir, "oath_tokens.csv")
+    oath_csv_path = os.path.join(
+        testcase.manage_ui.test_data_dir, "oath_tokens.csv"
+    )
     token_serials = (
         "tok1",
         "tok2",
@@ -125,27 +129,27 @@ def test_token_import_oath_csv(
     oathcsv_importer.do_import(file_path=oath_csv_path)
 
     # check the alert box
-    alert_box = AlertBoxHandler(manage_ui)
+    alert_box = AlertBoxHandler(testcase.manage_ui)
     assert (
         alert_box.last_line.text
         == "Token import result: 4 tokens were imported from the oath_tokens.csv file. OK"
     )
 
     # check the grid lines for the imported tokens
-    assert_tokens_are_in_grid(manage_ui, token_serials, token_types)
+    assert_tokens_are_in_grid(testcase.manage_ui, token_serials, token_types)
 
 
 def test_token_import_oath_csv_invalid_seed(
-    manage_ui: ManageUi, oathcsv_importer: TokenImportOATH
+    testcase, oathcsv_importer: TokenImportOATH
 ):
     "Test import of invalid oath csv tokens"
 
     # save the initial tokens for later comparison
-    tokens_at_first = manage_ui.token_view._get_token_list()
+    tokens_at_first = testcase.manage_ui.token_view._get_token_list()
 
     #   data to be tested against:
     oath_csv_path = os.path.join(
-        manage_ui.test_data_dir, "oath_tokens_bad_seed.csv"
+        testcase.manage_ui.test_data_dir, "oath_tokens_bad_seed.csv"
     )
     token_serials = (
         "tok1",
@@ -163,14 +167,16 @@ def test_token_import_oath_csv_invalid_seed(
     )
 
     # check the alert box showing the correct information
-    alert_box = AlertBoxHandler(manage_ui)
+    alert_box = AlertBoxHandler(testcase.manage_ui)
     assert (
         alert_box.last_line.text
         == "Failed to import token: InvalidSeedException('The provided token seed contains non-hexadecimal characters') OK"
     )
 
     # check that no token was imported
-    tokens_after_import_attempt = manage_ui.token_view._get_token_list()
+    tokens_after_import_attempt = (
+        testcase.manage_ui.token_view._get_token_list()
+    )
     tokens_at_first == tokens_after_import_attempt
 
 
