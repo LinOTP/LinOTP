4e7dd4895d17f37a8cff962a63a74a060872357e
Kay Winkler <kay.winkler@netgo.de>
2022-11-22 15:15:28 +0100

    chore: prepare release of LinOTP 3.2.1 rc0

--- api-doc/source/conf.py
+++ api-doc/source/conf.py
@@ -26,7 +26,7 @@ author = "netgo GmbH"
 # The short X.Y version
 version = "3.2"
 # The full version, including alpha/beta/rc tags
-release = "3.2.1.dev0"
+release = "3.2.1rc0"
 
 
 # -- General configuration ---------------------------------------------------
