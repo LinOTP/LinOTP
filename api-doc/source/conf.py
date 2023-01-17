361a195397beada6e2c96b7bd30059054f986188
Kay Winkler <kay.winkler@netgo.de>
2023-01-17 15:26:29 +0100

    chore: prepare for 3.2.3rc0 Release

--- api-doc/source/conf.py
+++ api-doc/source/conf.py
@@ -26,7 +26,7 @@ author = "netgo GmbH"
 # The short X.Y version
 version = "3.2"
 # The full version, including alpha/beta/rc tags
-release = "3.2.3.dev0"
+release = "3.2.3rc0"
 
 
 # -- General configuration ---------------------------------------------------
