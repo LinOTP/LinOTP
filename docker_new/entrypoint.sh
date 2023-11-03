ccb8d403ae9130d3de232d6f4d758aab330e92bc
Michael Weber <michael.weber@netgo.de>
2023-11-03 12:46:06 +0100

    refactor: move start/bootstrap into functions
    as we will call them in multiple cases soon

--- docker_new/entrypoint.sh
+++ docker_new/entrypoint.sh
@@ -15,6 +15,51 @@ set_admin_password_env() {
     echo >&2 "Please change it at your earliest convenience!"
 }
 
+bootstrap_linotp() {
+    echo >&2 "--- Bootstrapping LinOTP ---"
+    bootstrapped_file="$LINOTP_ROOT_DIR"/bootstrapped
+    if [ -f "$bootstrapped_file" ]; then
+        echo >&2 "Already bootstrapped - skipping"
+    else
+        linotp -v init database
+        linotp -v init audit-keys
+        linotp -v init enc-key
+        linotp -v local-admins add $LINOTP_ADMIN_USER
+        if [ -z "$LINOTP_ADMIN_PASSWORD" ]; then
+            set_admin_password_env
+        fi
+        linotp -v local-admins password --password $LINOTP_ADMIN_PASSWORD $LINOTP_ADMIN_USER
+        touch "$bootstrapped_file"
+    fi
+}
+
+start_linotp() {
+    echo >&2 "--- Starting LinOTP ---"
+    if [ "$MODE" = "production" ]; then
+        # linotp does currently not support multiple gunicorn workers
+        # due to its jwt and cookie handling.
+        # Once supported, set `--workers="${WORKER_PROCESSES:-1}"`
+        echo >&2 "Starting gunicorn on $SERVICE ..."
+        exec gunicorn \
+            --bind "${SERVICE}" --worker-tmp-dir=/dev/shm \
+            --workers=1 --threads="${WORKER_THREADS:-4}" \
+            --worker-class=gthread --log-file=- \
+            "linotpapp:create_app()"
+    elif [ "$MODE" = "development" ]; then
+        if [ -n "${I_KNOW_THIS_IS_BAD_AND_IF_TERRIBLE_THINGS_HAPPEN_IT_WILL_BE_MY_OWN_FAULT:-}" ]; then
+            echo >&2 "Starting development server on..."
+            echo >&2 "(DO NOT DO THIS FOR A PRODUCTION-GRADE SERVER!!!)"
+            exec linotp run
+        else
+            echo >&2 "You're in mode 'development' and did not set env I_KNOW_THIS_IS_BAD_AND_IF_TERRIBLE_THINGS_HAPPEN_IT_WILL_BE_MY_OWN_FAULT"
+            exit 1
+        fi
+    else
+        echo >&2 "Unsupported MODE: $MODE"
+        exit 1
+    fi
+}
+
 if [ -n "$LINOTP_DB_HOST" ]; then
     echo "Waiting for PostgreSQL..."
 
@@ -40,49 +85,12 @@ else
     echo >&2 "LINOTP_CFG is $file"
 fi
 
-
 if [ "$@" = "--with-bootstrap" ]; then
-    echo >&2 "--- Bootstrapping LinOTP ---"
-    bootstrapped_file="$LINOTP_ROOT_DIR"/bootstrapped
-    if [ -f "$bootstrapped_file" ]; then
-        echo >&2 "Already bootstrapped - skipping"
-    else
-        linotp -v init database
-        linotp -v init audit-keys
-        linotp -v init enc-key
-        linotp -v local-admins add $LINOTP_ADMIN_USER
-        if [ -z "$LINOTP_ADMIN_PASSWORD" ]; then
-            set_admin_password_env
-        fi
-        linotp -v local-admins password --password $LINOTP_ADMIN_PASSWORD $LINOTP_ADMIN_USER
-        touch "$LINOTP_ROOT_DIR"/bootstrapped
-    fi
+    bootstrap_linotp
 fi
+
 
 export MODE="${MODE:-production}"
 export SERVICE="${SERVICE:-0.0.0.0:5000}"
 
-echo >&2 "--- Starting LinOTP ---"
-if [ "$MODE" = "production" ]; then
-    # linotp does currently not support multiple gunicorn workers
-    # due to its jwt and cookie handling.
-    # Once supported, set `--workers="${WORKER_PROCESSES:-1}"`
-    echo >&2 "Starting gunicorn on $SERVICE ..."
-    exec gunicorn \
-        --bind "${SERVICE}" --worker-tmp-dir=/dev/shm \
-        --workers=1 --threads="${WORKER_THREADS:-4}" \
-        --worker-class=gthread --log-file=- \
-        "linotpapp:create_app()"
-elif [ "$MODE" = "development" ] \
-	 && [ -n "$I_KNOW_THIS_IS_BAD_AND_IF_TERRIBLE_THINGS_HAPPEN_IT_WILL_BE_MY_OWN_FAULT" ]; then
-    echo >&2 "Starting development server on..."
-    echo >&2 "(DO NOT DO THIS FOR A PRODUCTION-GRADE SERVER!!!)"
-    exec linotp run
-    else
-        echo >&2 "You're in mode 'development' and did not set env I_KNOW_THIS_IS_BAD_AND_IF_TERRIBLE_THINGS_HAPPEN_IT_WILL_BE_MY_OWN_FAULT"
-        exit 1
-    fi
-else
-    echo >&2 "Unsupported MODE: $MODE"
-    exit 1
-fi
+start_linotp
