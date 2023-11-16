59ebfaeece9089882bdbd1af9e76e59d63ea0562
Michael Weber <michael.weber@netgo.de>
2023-11-16 14:15:16 +0100

    chore: set version to 3.2.5rc0

--- api-doc/source/conf.py
+++ api-doc/source/conf.py
@@ -26,7 +26,7 @@ author = "netgo software GmbH"
 # The short X.Y version
 version = "3.2"
 # The full version, including alpha/beta/rc tags
-release = "3.2.4"
+release = "3.2.5rc0"
 
 
 # -- General configuration ---------------------------------------------------
