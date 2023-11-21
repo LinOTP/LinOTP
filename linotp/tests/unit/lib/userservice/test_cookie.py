a51e7b0180611e7a202d72581155a56a00283eb4
Anselm Lingnau <anselm.lingnau@netgo.de>
2023-11-21 12:31:10 +0100

    fix: generate userservice session cookie values entirely random
    Previously, there was a possibility of different userservice sessions
    sharing the same session cookie, if the same user started several
    sessions nearly simultaneously.
    
    The update generates session cookie values by picking a random value
    instead of the previous mechanism based on hashing known data.

--- linotp/tests/unit/lib/userservice/test_cookie.py
+++ linotp/tests/unit/lib/userservice/test_cookie.py
@@ -26,7 +26,6 @@
 #
 " verfify that the userservice cookie is rfc6265 time format compliant"
 
-import binascii
 import datetime
 import unittest
 
@@ -36,24 +35,17 @@ from mock import patch
 from linotp.lib.user import User
 from linotp.lib.userservice import create_auth_cookie
 
-Secret = "012345678901234567890123456789012"
 RFC6265_TIMEFORMAT = "%a, %d %b %Y %H:%M:%S GMT"
 OLD_TIMEFORMAT = "%Y-%m-%d %H:%M:%S"
 
 
 class TestCookieActivation(unittest.TestCase):
-    @patch("linotp.lib.userservice.get_cookie_secret")
     @patch("linotp.lib.userservice.get_cookie_expiry")
-    def test_tz_in_cookies(
-        self, mock_get_cookie_expiry, mock_get_cookie_secret
-    ):
+    def test_tz_in_cookies(self, mock_get_cookie_expiry):
         """
         verify that the userservice cookie format is rfc6265 compliant
         """
 
-        mock_get_cookie_secret.return_value = binascii.hexlify(
-            Secret.encode("utf-8")
-        )
         mock_get_cookie_expiry.return_value = False
 
         ret = create_auth_cookie(User("hans"), "127.0.0.1")
