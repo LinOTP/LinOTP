596f8eca2a9963ade56c917104078cf323a30ee7
Lukas Engelter <lukas.engelter@netgo.de>
2023-11-24 14:49:00 +0100

    chore: fix missed rc tag in python package version
    chore: fix missed rc tag in api-docs version

--- api-doc/source/conf.py
+++ api-doc/source/conf.py
@@ -26,7 +26,7 @@ author = "netgo software GmbH"
 # The short X.Y version
 version = "3.2"
 # The full version, including alpha/beta/rc tags
-release = "3.2.5rc1"
+release = "3.2.5"
 
 
 # -- General configuration ---------------------------------------------------
