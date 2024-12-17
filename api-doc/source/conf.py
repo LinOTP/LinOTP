d2bb901d9b206d2ce8852c7308329d8d0ae83655
Michael Weber <michael.weber@netgo.de>
2024-12-17 12:19:05 +0100

    chore: bump version to 3.4.2dev0

--- api-doc/source/conf.py
+++ api-doc/source/conf.py
@@ -26,7 +26,7 @@ author = "netgo software GmbH"
 # The short X.Y version
 version = "3.4"
 # The full version, including alpha/beta/rc tags
-release = "3.4.1"
+release = "3.4.2.dev0"
 
 # -- General configuration ---------------------------------------------------
 
