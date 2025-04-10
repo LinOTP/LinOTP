62961a02057db09cd72e373110c644911ab3a39f
Lukas Engelter <lukas.engelter@netgo.de>
2025-04-10 19:03:25 +0200

    style: autoformat auth.js using prettier

--- linotp/public/js/auth.js
+++ linotp/public/js/auth.js
@@ -26,258 +26,252 @@
  */
 
 function clientUrlFetchSync(myUrl, params) {
-    /*
-     * clientUrlFetchSync - to submit a syncronous  http request
-     *
-     * @remark: introduced the params (:dict:) so we could switch to
-     *          a POST request, which will allow more and secure data
-     */
+  /*
+   * clientUrlFetchSync - to submit a syncronous  http request
+   *
+   * @remark: introduced the params (:dict:) so we could switch to
+   *          a POST request, which will allow more and secure data
+   */
 
-    var resp = $.ajax({
-        url : myUrl,
-        data : params,
-        async : false,
-        type : 'POST',
-    }).responseText;
+  var resp = $.ajax({
+    url: myUrl,
+    data: params,
+    async: false,
+    type: "POST",
+  }).responseText;
 
-    return resp;
+  return resp;
 }
 
 function getOcra2Challenge() {
-    var user = $('#user').val();
-    var targetId = 'display';
-    var userId = 'user2';
+  var user = $("#user").val();
+  var targetId = "display";
+  var userId = "user2";
 
-    var params = {};
-    params['user'] = $('#user').val();
-    params['pass'] = $('#pin').val();
-    params['data'] = $('#challenge').val();
-    params['qr'] = 'img';
+  var params = {};
+  params["user"] = $("#user").val();
+  params["pass"] = $("#pin").val();
+  params["data"] = $("#challenge").val();
+  params["qr"] = "img";
 
-    var url = '/validate/check';
+  var url = "/validate/check";
 
-    try {
-        var data = clientUrlFetchSync(url, params);
-        if ( typeof (data) == "object") {
-            var err = data.result.error.message;
-            alert(err);
-        } else {
-            var img = data;
-            $('#' + targetId).html(img);
-            $('#' + userId).val(user);
-        }
-    } catch (e) {
-        alert(e);
+  try {
+    var data = clientUrlFetchSync(url, params);
+    if (typeof data == "object") {
+      var err = data.result.error.message;
+      alert(err);
+    } else {
+      var img = data;
+      $("#" + targetId).html(img);
+      $("#" + userId).val(user);
     }
+  } catch (e) {
+    alert(e);
+  }
 }
 
 function triggerChallenge() {
-    var user = $('#user').val();
-    var pin = $('#pin').val();
-    var data = $('#challenge').val();
+  var user = $("#user").val();
+  var pin = $("#pin").val();
+  var data = $("#challenge").val();
 
-    var $target = $('#display');
-    var $transidInput = $('#transactionid');
-    var $otpInput = $('#otp');
+  var $target = $("#display");
+  var $transidInput = $("#transactionid");
+  var $otpInput = $("#otp");
 
-    var params = {};
-    params['user'] = user;
-    params['pass'] = pin;
-    params['data'] = data;
-    params['qr'] = 'html';
-    params['content_type'] = 0;
+  var params = {};
+  params["user"] = user;
+  params["pass"] = pin;
+  params["data"] = data;
+  params["qr"] = "html";
+  params["content_type"] = 0;
 
-    var url = '/validate/check';
+  var url = "/validate/check";
+
+  try {
+    var data = clientUrlFetchSync(url, params);
 
     try {
-        var data = clientUrlFetchSync(url, params);
+      var json = typeof data == "object" ? data : JSON.parse(data);
 
-        try {
-            var json = (typeof(data) == "object") ? data : JSON.parse(data);
+      var message;
 
-            var message;
+      if (json.result && !json.result.detail) {
+        message = "Failed to trigger challenge";
+        if (json.result.error && json.result.error.message) {
+          message += "\n" + json.result.error.message;
+        }
+      } else {
+        message = "response format unexpected";
+      }
 
-            if(json.result && !json.result.detail) {
-                message = "Failed to trigger challenge";
-                if(json.result.error && json.result.error.message) {
-                    message += "\n" + json.result.error.message;
-                }
-            }
-            else {
-                message = "response format unexpected";
-            }
+      alert(message);
+      return;
+    } catch (e) {}
 
-            alert(message);
-            return;
+    var htmlResponse = $(data);
 
-        } catch(e) {}
+    var img = htmlResponse.find("#challenge_qrcode");
+    var lseqrurl = img.attr("alt");
+    var transactionid = htmlResponse
+      .find("#challenge_data .transactionid")
+      .text();
 
-        var htmlResponse = $(data);
+    $target.html(img);
+    $target.append("<p>" + decodeURIComponent(lseqrurl) + "</p>");
+    $transidInput.val(transactionid);
 
-        var img = htmlResponse.find('#challenge_qrcode');
-        var lseqrurl = img.attr("alt");
-        var transactionid = htmlResponse.find('#challenge_data .transactionid').text();
-
-        $target.html(img);
-        $target.append("<p>" + decodeURIComponent(lseqrurl) + "</p>");
-        $transidInput.val(transactionid);
-
-        $otpInput.val("").focus();
-
-    } catch (e) {
-        alert(e);
-    }
+    $otpInput.val("").focus();
+  } catch (e) {
+    alert(e);
+  }
 }
 
 function check_status() {
-    var user = $('#user').val();
-    var pin = $('#pin').val();
-    var transactionid = $('#transactionid').val();
+  var user = $("#user").val();
+  var pin = $("#pin").val();
+  var transactionid = $("#transactionid").val();
 
-    var params = {};
-    params['user'] = user;
-    params['pass'] = pin;
-    params['transactionid'] = transactionid;
+  var params = {};
+  params["user"] = user;
+  params["pass"] = pin;
+  params["transactionid"] = transactionid;
 
-    var url = '/validate/check_status';
+  var url = "/validate/check_status";
 
-    var resp = clientUrlFetchSync(url, params);
-    var data = jQuery.parseJSON(resp);
+  var resp = clientUrlFetchSync(url, params);
+  var data = jQuery.parseJSON(resp);
 
-    if (false === data.result.status) {
-        alert(data.result.error.message);
-    } else {
-        if (true === data.result.value) {
-            if(data.detail.transactions[transactionid]["received_tan"] === true){
-                if(data.detail.transactions[transactionid]["valid_tan"] === true){
-                    alert("User successfully authenticated!");
-                }
-                else {
-                    alert("User rejected the authentication request");
-                }
-            }
-            else if(data.detail.transactions[transactionid]["received_tan"] === false){
-                alert("User did not authenticate yet!");
-            }
-            else{
-                alert("Error during request! Check for challenge timeout...");
-            }
-        } else if ("detail" in data && "message" in data.detail) {
-            alert(data.detail.message)
+  if (false === data.result.status) {
+    alert(data.result.error.message);
+  } else {
+    if (true === data.result.value) {
+      if (data.detail.transactions[transactionid]["received_tan"] === true) {
+        if (data.detail.transactions[transactionid]["valid_tan"] === true) {
+          alert("User successfully authenticated!");
         } else {
-            alert("Error during request! Check for challenge timeout...");
+          alert("User rejected the authentication request");
         }
+      } else if (
+        data.detail.transactions[transactionid]["received_tan"] === false
+      ) {
+        alert("User did not authenticate yet!");
+      } else {
+        alert("Error during request! Check for challenge timeout...");
+      }
+    } else if ("detail" in data && "message" in data.detail) {
+      alert(data.detail.message);
+    } else {
+      alert("Error during request! Check for challenge timeout...");
     }
+  }
 }
 
 function submitChallengeResponse() {
-    var user = $('#user').val();
-    var otp = $('#otp').val();
-    var transactionid = $('#transactionid').val();
+  var user = $("#user").val();
+  var otp = $("#otp").val();
+  var transactionid = $("#transactionid").val();
 
-    var params = {};
-    params['user'] = user;
-    params['pass'] = otp;
-    params['transactionid'] = transactionid;
+  var params = {};
+  params["user"] = user;
+  params["pass"] = otp;
+  params["transactionid"] = transactionid;
 
-    var url = '/validate/check';
+  var url = "/validate/check";
 
-    var resp = clientUrlFetchSync(url, params);
-    var data = jQuery.parseJSON(resp);
+  var resp = clientUrlFetchSync(url, params);
+  var data = jQuery.parseJSON(resp);
 
-    if (false == data.result.status) {
-        alert(data.result.error.message);
+  if (false == data.result.status) {
+    alert(data.result.error.message);
+  } else {
+    if (true == data.result.value) {
+      alert("User successfully authenticated!");
+    } else if ("detail" in data && "message" in data.detail) {
+      alert(data.detail.message);
     } else {
-        if (true == data.result.value) {
-            alert("User successfully authenticated!");
-        } else if ("detail" in data && "message" in data.detail) {
-            alert(data.detail.message)
-        } else {
-            alert("User failed to authenticate!");
-        }
+      alert("User failed to authenticate!");
     }
+  }
 }
 
 function login_user(column) {
-    var user = "";
-    var pass = "";
-    if (column == 3) {
-        user = $('#user3').val();
-        pass = encodeURIComponent($('#pass3').val() + $('#otp3').val());
+  var user = "";
+  var pass = "";
+  if (column == 3) {
+    user = $("#user3").val();
+    pass = encodeURIComponent($("#pass3").val() + $("#otp3").val());
+  } else {
+    user = $("#user").val();
+    pass = $("#pass").val();
+  }
+
+  var params = {};
+  params["user"] = user;
+  params["pass"] = pass;
+
+  var resp = clientUrlFetchSync("/validate/check", params);
+  var data = jQuery.parseJSON(resp);
+
+  if (false == data.result.status) {
+    alert(data.result.error.message);
+  } else {
+    if (true == data.result.value) {
+      alert("User successfully authenticated!");
+    } else if ("detail" in data && "message" in data.detail) {
+      alert(data.detail.message);
     } else {
-        user = $('#user').val();
-        pass = $('#pass').val();
+      alert("User failed to authenticate!");
     }
-
-    var params = {};
-    params['user'] = user;
-    params['pass'] = pass;
-
-    var resp = clientUrlFetchSync('/validate/check', params);
-    var data = jQuery.parseJSON(resp);
-
-    if (false == data.result.status) {
-        alert(data.result.error.message);
-    } else {
-        if (true == data.result.value) {
-            alert("User successfully authenticated!");
-        } else if ("detail" in data && "message" in data.detail) {
-            alert(data.detail.message)
-        } else {
-            alert("User failed to authenticate!");
-        }
-        //$('#user').val('');
-        $('#pass').val('');
-        $('#otp3').val('');
-    }
-
+    //$('#user').val('');
+    $("#pass").val("");
+    $("#otp3").val("");
+  }
 }
 
-$(document).ready(function() {
+$(document).ready(function () {
+  $("button, input[type=submit]").button();
 
-    $("button, input[type=submit]").button();
+  /*
+   * Auth login callbacks
+   */
 
-    /*
-    * Auth login callbacks
-    */
+  // auth/index
+  $("#form_login").submit(function (submit_event) {
+    submit_event.preventDefault();
+    login_user((column = 2));
+  });
 
-    // auth/index
-    $("#form_login").submit(function(submit_event) {
-        submit_event.preventDefault();
-        login_user( column = 2);
-    });
+  // auth/index3
+  $("#form_login3").submit(function (submit_event) {
+    submit_event.preventDefault();
+    login_user((column = 3));
+  });
 
-    // auth/index3
-    $("#form_login3").submit(function(submit_event) {
-        submit_event.preventDefault();
-        login_user( column = 3);
-    });
+  // auth/qrtoken
+  $("#form_challenge_trigger").submit(function (e) {
+    e.preventDefault();
+    triggerChallenge();
+  });
 
-    // auth/qrtoken
-    $("#form_challenge_trigger").submit(function(e) {
-        e.preventDefault();
-        triggerChallenge();
-    });
+  $("#form_challenge_submit").submit(function (e) {
+    e.preventDefault();
+    submitChallengeResponse();
+  });
 
-    $("#form_challenge_submit").submit(function(e) {
-        e.preventDefault();
-        submitChallengeResponse();
-    });
+  $("#check_status").click(function (click_event) {
+    check_status();
+  });
 
-    $("#check_status").click(function(click_event) {
-        check_status();
-    });
+  // auth/ocra2
+  $("#form_challenge_ocra2").submit(function (submit_event) {
+    submit_event.preventDefault();
+    getOcra2Challenge();
+  });
 
-    // auth/ocra2
-    $("#form_challenge_ocra2").submit(function(submit_event) {
-        submit_event.preventDefault();
-        getOcra2Challenge();
-    });
-
-    $("#form_login_ocra2").submit(function(submit_event) {
-        submit_event.preventDefault();
-        login_user( column = 2);
-    });
-
+  $("#form_login_ocra2").submit(function (submit_event) {
+    submit_event.preventDefault();
+    login_user((column = 2));
+  });
 });
-
